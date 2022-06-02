b122f9b0e3d97f4660ab6bfa9a5c8f42d9bfb62d
Bharat Rathod <94593235+Bharatrathod-81@users.noreply.github.com>
2022-06-02 11:00:40 +0530

    Update nav-bar.jsx

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -8,8 +8,8 @@ export const NavBar = () => {
         <div className="nav-body align-centre jstfy-spce-around">
             <i className="fa fa-bars menu-btn margin-small"></i>
             <div className="nav-items jstfy-centre align-centre jstfy-spce-btwn">
-                <h1><Link to="/"><span className="logo1 padding-small">VIRTUAL</span><span className="logo2">tube</span></Link></h1>
-                <button className="login-btn padding-small margin-small">LOGIN</button>
+                <h1 className="margin-Xsmall"><Link to="/"><span className="logo1 padding-small">VIRTUAL</span><span className="logo2">tube</span></Link></h1>
+                <button className="login-btn padding-small margin-Xsmall">LOGIN</button>
             </div>
         </div>
     )
