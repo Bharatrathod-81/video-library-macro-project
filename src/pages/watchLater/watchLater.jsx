8782d80112c82a644e69d24c1eef6f540405cb7a
Bharat Rathod <94593235+Bharatrathod-81@users.noreply.github.com>
2022-06-08 21:15:37 +0530

    Update watchLater.jsx

--- src/pages/watchLater/watchLater.jsx
+++ src/pages/watchLater/watchLater.jsx
@@ -19,15 +19,17 @@ export const WatchLater = () => {
         <div className="jstfy-centre flex-wrap">
             {WatchLater.map(item => {
                 return (
-                    <div
-                        key={item._id}
-                        className="margin-small watch-card-body ">
-                        <Card data={item} />
-                        <button
-                            onClick={() => dispatch(deleteWatchLater(item))}
-                            className="watch-remove-btn padding-small"
-                        >Remove From Watch Later</button>
-                    </div>
+                    <Link to={`/singleVideo/${item._id}`}>
+                        <div
+                            key={item._id}
+                            className="margin-small watch-card-body ">
+                            <Card data={item} />
+                            <button
+                                onClick={() => dispatch(deleteWatchLater(item))}
+                                className="watch-remove-btn padding-small"
+                            >Remove From Watch Later</button>
+                        </div>
+                    </Link>
                 )
             })}
         </div>
