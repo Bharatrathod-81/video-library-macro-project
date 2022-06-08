84bc2c9010123977a47e14a50a076c8037c8f233
Bharat Rathod <94593235+Bharatrathod-81@users.noreply.github.com>
2022-06-08 10:43:20 +0530

    Update side-bar.jsx

--- src/components/side-bar/side-bar.jsx
+++ src/components/side-bar/side-bar.jsx
@@ -10,7 +10,7 @@ export const SideBar = () => {
     return (
         <div className={toggle ?"side-bar-body flex-column":"remove-sideBar"}>
             <div className="align-start flex-column">
-                <h4 className="sideBar-item"><i className="fa fa-home"></i> HOME</h4>
+                <h4 className="sideBar-item"><Link to="/"><i className="fa fa-home"></i> HOME</Link></h4>
                 <h4 className="sideBar-item"><Link to="/playPage/All"><i className="fa fa-tv"></i> EXPLORE</Link></h4>
                 <h4 className="sideBar-item"><i className="fa fa-toggle-right"></i> PLAYLIST</h4>
                 <h4 className="sideBar-item"><i className="fa fa-thumbs-up"></i> LIKE</h4>
